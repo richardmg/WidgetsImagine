d1c9a5b2278d9704de71eb0563843a217c43489a
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2022-01-26 16:52:44 +0100

    cache nine patch images in a map

--- main.cpp
+++ main.cpp
@@ -12,18 +12,52 @@ class MyProxyStyle : public QProxyStyle
 {
   public:
 
+    MyProxyStyle()
+    {
+        loadAndCacheImages();
+    }
+
+    ~MyProxyStyle() {
+        qDeleteAll(m_ninePatchImages);
+    }
+
+    void loadAndCacheImages()
+    {
+        // TODO: remove duplicates, only cache images of correct size (@2x, @3x etc).
+        QDirIterator it(QStringLiteral(":/images"), { "*.9.png" }, QDir::Files);
+        while (it.hasNext()) {
+            const QString fileName = it.next();
+            const QImage image(fileName);
+            try {
+                // does this leak if exception is thrown?
+                TNinePatch *npImage = new TNinePatch(image);
+                m_ninePatchImages.insert(fileName, npImage);
+            } catch (NinePatchException *exception) {
+                qDebug() << "load, exception:" << exception->what();
+            }
+        }
+    }
+
     void drawControl(QStyle::ControlElement element, const QStyleOption *option, QPainter *painter, const QWidget *widget = nullptr) const override
     {
         switch (element) {
         case CE_PushButton: {
-            QImage image(QStringLiteral(":/images/button-background.9.png"));
-            TNinePatch npImage(image);
-            npImage.draw(*painter, option->rect);
+            if (TNinePatch *npImage = m_ninePatchImages[":/images/button-background.9.png"]) {
+                npImage->draw(*painter, option->rect);
+
+                if (const QStyleOptionButton *btn = qstyleoption_cast<const QStyleOptionButton *>(option)) {
+                    QStyleOptionButton subopt = *btn;
+                    subopt.rect = subElementRect(SE_PushButtonContents, btn, widget);
+                    proxy()->drawControl(CE_PushButtonLabel, &subopt, painter, widget);
+                }
+                return;
+            }
             break; }
         default:
-            QProxyStyle::drawControl(element, option, painter, widget);
             break;
         }
+
+        QProxyStyle::drawControl(element, option, painter, widget);
     }
 
     QSize sizeFromContents(QStyle::ContentsType type, const QStyleOption *option, const QSize &size, const QWidget *widget) const override
@@ -42,16 +76,11 @@ class MyProxyStyle : public QProxyStyle
         }
         return widgetSize;
     }
-};
 
-void loadAndCacheImages()
-{
-    QDirIterator it(QStringLiteral(":/images"), { "*.png" }, QDir::Files);
-    while (it.hasNext()) {
-        const QString fileName = it.next();
-        qDebug() << fileName;
-    }
-}
+private:
+    QMap<QString, TNinePatch*> m_ninePatchImages;
+
+};
 
 int main(int argc, char *argv[])
 {
